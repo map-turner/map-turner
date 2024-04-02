a9e3ae978169df137c02be89851b2ce76e61009b
Damien Rembert <damien.rembert@gmail.com>
2024-04-03 00:13:22 +0100

    add helper files

---
+++ src/utils/coordinate.js
@@ -0,0 +1,4 @@
+// get coordinates from postcode
+// add "noise" to coordinates (can't be random or running things multiple times will allow you to guess location)
+// use fakerjs?
+const ltlPostcode = "L8 0SR";
