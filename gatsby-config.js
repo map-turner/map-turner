ebaa0bf56c6628ea27688947187467836c20f8b1
Damien Rembert <damien.rembert@gmail.com>
2024-04-02 21:17:56 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `LTL Maps`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
