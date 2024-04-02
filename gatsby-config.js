ec4e9e9d931a0ae380142a18d7ca04a8a2c5be9f
Damien Rembert <damien.rembert@gmail.com>
2024-04-02 21:54:41 +0100

    add leaflet

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,11 @@ module.exports = {
     title: `LTL Maps`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-react-leaflet',
+      options: {
+        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
+      }
+    }
+  ]}
