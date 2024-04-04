d5f9bb58979f4c8456501791bc743a950e33459f
Damien Rembert <damien.rembert@gmail.com>
2024-04-04 23:24:03 +0100

    use data from csv

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,6 +1,7 @@
 // import React from "react";
 import * as React from "react";
 import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
+import { graphql } from "gatsby";
 
 const toolLibrary = [53.3939431, -2.9486509, 15];
 
@@ -28,7 +29,9 @@ const users = [
   },
 ];
 
-export default function IndexPage() {
+const MapPage = ({ data }) => {
+  const circles = data.allPostcodesCsv.edges;
+
   return (
     <MapContainer
       // style={{ height: "600px" }}
@@ -46,16 +49,33 @@ export default function IndexPage() {
           The Tool Library. <br /> That's where the tools are.
         </Popup>
       </Marker>
-      {users.map((user) => (
+      {circles.map((circle) => (
         <Circle
-          center={user.position}
-          radius={user.radius}
-          opacity={user.opacity}
-          color={user.color}
+          key={circle.node.postcode}
+          center={[circle.node.latitude, circle.node.longitude]}
+          radius="50"
+          opacity="0.7"
+          color="#3388ff"
         >
-          <Popup>{user.info}</Popup>
+          <Popup>{circle.node.Count_sum}</Popup>
         </Circle>
       ))}
     </MapContainer>
   );
-}
+};
+
+export const query = graphql`
+  query MyQuery {
+    allPostcodesCsv {
+      edges {
+        node {
+          Count_sum
+          latitude
+          longitude
+          postcode
+        }
+      }
+    }
+  }
+`;
+export default MapPage;
