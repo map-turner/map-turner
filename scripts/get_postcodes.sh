b46167e3084875b144fe430fd458b2f15202b328
Damien R <damien.rembert@gmail.com>
2024-04-28 14:51:49 +0100

    use file with all postcodes

--- scripts/get_postcodes.sh
+++ scripts/get_postcodes.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo 'postcode,status,usertype,easting,northing,positional_quality_indicator,country,latitude,longitude,postcode_no_space,postcode_fixed_width_seven,postcode_fixed_width_eight,postcode_area,postcode_district,postcode_sector,outcode,incode' > postcodes.csv
-curl https://www.getthedata.com/downloads/open_postcode_geo_merseyside.csv >> postcodes.csv
+
+# If you're in the UK, this works but it's quite slow as the file has so many rows.
+curl https://www.getthedata.com/downloads/open_postcode_geo.csv.tar.gz --output open_postcode_geo.csv.tar.gz
+tar -zxvf open_postcode_geo.csv.tar.gz open_postcode_geo.csv
+cat open_postcode_geo.csv >> postcodes.csv
+
+# If you're lucky, regional extracts might be available see https://www.getthedata.com/open-postcode-geo eg for Merseyside:
+# curl https://www.getthedata.com/downloads/open_postcode_geo_merseyside.csv >> postcodes.csv
